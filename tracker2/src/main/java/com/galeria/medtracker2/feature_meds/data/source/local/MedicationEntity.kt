590fadf766b5eea5d01ebee345c3077bab9267b4
cocaine <zhenya.krylov96@gmail.com>
2025-12-16 00:19:30 -0500

    .

--- tracker2/src/main/java/com/galeria/medtracker2/feature_meds/data/source/local/MedicationEntity.kt
+++ tracker2/src/main/java/com/galeria/medtracker2/feature_meds/data/source/local/MedicationEntity.kt
@@ -7,6 +7,9 @@ import androidx.room.PrimaryKey
 data class MedicationEntity(
     @PrimaryKey val id: String,
     val name: String,
-    val stock: Double?,
-    val measureUnit: String?,
+    // В 1 единице (таблетке), для не мед препаратов мб упустить.
+    val doseMg: Double?,
+    val stock: Double?, // Кол-во штук/грамм..
+    val stockMeasureUnit: String?, // Показывает это штуки или граммы.
+    val drugClass: String?, // stim/opioid/benz..
 )
