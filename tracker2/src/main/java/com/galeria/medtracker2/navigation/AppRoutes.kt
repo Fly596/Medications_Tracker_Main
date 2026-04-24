f2e0bc4a3f3a62e0f55842bdaaa1c24a978d2954
bob <fly.yt.77@gmail.com>
2026-04-24 06:43:11 -0500

    .

--- tracker2/src/main/java/com/galeria/medtracker2/navigation/AppRoutes.kt
+++ tracker2/src/main/java/com/galeria/medtracker2/navigation/AppRoutes.kt
@@ -5,28 +5,19 @@ import kotlinx.serialization.Serializable
 @Serializable
 sealed interface Graph {
 
-    @Serializable data object Auth : Graph
-
-    @Serializable data object App : Graph
-}
-
-// --- Уровень 2: Маршруты Экранов (Сгруппированные) ---
-@Serializable
-sealed interface AuthScreen {
-
-    @Serializable data object Login : AuthScreen
-
-    @Serializable data object Registration : AuthScreen
-
-    @Serializable data object RestorePassword : AuthScreen
+    @Serializable
+    data object App : Graph
 }
 
 @Serializable
 sealed interface AppScreen {
 
-    @Serializable data class Home(val userId: String = "") : AppScreen
+    @Serializable
+    data object Home : AppScreen
 
-    @Serializable data object Medications : AppScreen
+    @Serializable
+    data object Medications : AppScreen
 
-    @Serializable data object Account : AppScreen
+    @Serializable
+    data object Account : AppScreen
 }
